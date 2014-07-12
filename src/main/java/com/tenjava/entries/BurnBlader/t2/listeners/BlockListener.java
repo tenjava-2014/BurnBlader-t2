d3ba9f92f7154f29d88349c336a98901ac5fa266
contact@burnblader.me <contact@burnblader.me>
2014-07-12 16:48:52 +0100

    Added stone to power ups.

--- src/main/java/com/tenjava/entries/BurnBlader/t2/listeners/BlockListener.java
+++ src/main/java/com/tenjava/entries/BurnBlader/t2/listeners/BlockListener.java
@@ -1,5 +1,6 @@
 package com.tenjava.entries.BurnBlader.t2.listeners;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.bukkit.Bukkit;
@@ -12,9 +13,11 @@ import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.util.Vector;
 
 import com.tenjava.entries.BurnBlader.t2.TenJava;
 import com.tenjava.entries.BurnBlader.t2.energy.Energy;
@@ -23,6 +26,8 @@ import com.tenjava.entries.BurnBlader.t2.utils.Chat;
 public class BlockListener implements Listener {
 	
 	private HashMap<String, Integer> sneakingTasks = new HashMap<String, Integer>();
+	
+	private ArrayList<String> fallStomp = new ArrayList<String>();
 	
 	@EventHandler
 	public void onSneak(final PlayerToggleSneakEvent event) {
@@ -57,7 +62,32 @@ public class BlockListener implements Listener {
 		}
 	}
 	
-	public void block(Player player, Material b) {
+	@EventHandler
+	public void onDamage(EntityDamageEvent event) {
+		if(event.getEntity() instanceof Player) {
+			Player player = (Player) event.getEntity();
+			if(fallStomp.contains(player.getName())) {
+				event.setDamage(0.0);
+				Block b = player.getLocation().getBlock().getRelative(BlockFace.DOWN);
+				b.getRelative(BlockFace.DOWN).setType(b.getType());
+				b.setType(Material.AIR);
+				Block b1 = b.getRelative(BlockFace.EAST);
+				b1.getRelative(BlockFace.DOWN).setType(b1.getType());
+				b1.setType(Material.AIR);
+				Block b2 = b.getRelative(BlockFace.WEST);
+				b2.getRelative(BlockFace.DOWN).setType(b2.getType());
+				b2.setType(Material.AIR);
+				Block b3 = b.getRelative(BlockFace.NORTH);
+				b3.getRelative(BlockFace.DOWN).setType(b3.getType());
+				b3.setType(Material.AIR);
+				Block b4 = b.getRelative(BlockFace.SOUTH);
+				b4.getRelative(BlockFace.DOWN).setType(b4.getType());
+				b4.setType(Material.AIR);
+			}
+		}
+	}
+	
+	public void block(final Player player, Material b) {
 		if(b == Material.DIAMOND_ORE) {
 			if(Energy.get(player) >= 30) {
 				player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 20 * 30, 5));
@@ -85,11 +115,35 @@ public class BlockListener implements Listener {
 				Chat.sendMessage(player, Chat.NOT_ENOUGH_ENERGY.replace("$", "40"));
 			}
 		} else if(b == Material.WOOL) {
-			if(Energy.get(player) >= 40) {
+			if(Energy.get(player) >= 10) {
 				player.teleport(player.getBedSpawnLocation());
-				Energy.remove(player, 40);
+				Energy.remove(player, 10);
 			} else {
-				Chat.sendMessage(player, Chat.NOT_ENOUGH_ENERGY.replace("$", "40"));
+				Chat.sendMessage(player, Chat.NOT_ENOUGH_ENERGY.replace("$", "10"));
+			}
+		} else if(b == Material.STONE) {
+			if(Energy.get(player) >= 50) {
+				player.setVelocity(new Vector(0, 4, 0));
+				fallStomp.add(player.getName());
+				Bukkit.getScheduler().runTaskLater(TenJava.get(), new Runnable() {
+
+					@Override
+					public void run() {
+						player.setVelocity(new Vector(0, -5, 0));
+					}
+					
+				}, 20L);
+				Bukkit.getScheduler().runTaskLater(TenJava.get(), new Runnable() {
+
+					@Override
+					public void run() {
+						fallStomp.remove(player.getName());
+					}
+					
+				}, 100L);
+				Energy.remove(player, 50);
+			} else {
+				Chat.sendMessage(player, Chat.NOT_ENOUGH_ENERGY.replace("$", "10"));
 			}
 		} else {
 			Chat.sendMessage(player, ChatColor.RED + "This block does nothing...");
