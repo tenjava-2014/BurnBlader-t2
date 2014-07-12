a9c6fc98d3e4b5ffd3ca35d2de08b5b0c3d71363
oisinaylward@gmail.com <oisinaylward@gmail.com>
2014-07-12 11:47:34 +0100

    Revert "Added Log class & the base of the TenJava class."
    This reverts commit 5367fcd9fae0461c6c54c27ebb2f148314ee4447.

--- src/main/java/com/tenjava/entries/BurnBlader/t2/TenJava.java
+++ src/main/java/com/tenjava/entries/BurnBlader/t2/TenJava.java
@@ -1,28 +1,5 @@
 package com.tenjava.entries.BurnBlader.t2;
 
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.tenjava.entries.BurnBlader.t2.utils.Log;
-
-public class TenJava extends JavaPlugin {
-	
-	public void onEnable() {
-		Log.info("BurnBlader - TenJava Entry", "Starting up...");
-		registerListeners(getServer().getPluginManager());
-		registerCommands();
-	}
-	
-	public void onDisable() {
-		Log.info("BurnBlader - TenJava Entry", "Closing down...");
-	}
-	
-	void registerListeners(PluginManager pm) {
-		
-	}
-	
-	void registerCommands() {
-		
-	}
-	
-}
+public class TenJava extends JavaPlugin {}
