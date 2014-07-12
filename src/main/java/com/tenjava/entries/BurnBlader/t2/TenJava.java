69dc4464d7c5e7e40fb1d68daee39aa8dfce4262
contact@burnblader.me <contact@burnblader.me>
2014-07-12 11:54:08 +0100

    Added Log class & added base to TenJava class.

--- src/main/java/com/tenjava/entries/BurnBlader/t2/TenJava.java
+++ src/main/java/com/tenjava/entries/BurnBlader/t2/TenJava.java
@@ -1,5 +1,28 @@
 package com.tenjava.entries.BurnBlader.t2;
 
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+import com.tenjava.entries.BurnBlader.t2.utils.Log;
+
+public class TenJava extends JavaPlugin {
+	
+	public void onEnable() {
+		Log.info("BurnBlader TenJava Entry", "Starting up...");
+		registerListeners(getServer().getPluginManager());
+		registerCommands();
+	}
+	
+	public void onDisable() {
+		Log.info("BurnBlader TenJava Entry", "Closing down...");
+	}
+	
+	void registerListeners(PluginManager pm) {
+		
+	}
+	
+	void registerCommands() {
+		
+	}
+	
+}
